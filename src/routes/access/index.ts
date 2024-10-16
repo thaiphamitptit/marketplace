69d680b8f9f09be6cfb020a248e28ff97f95b36d
thaiphamitptit <phamthai.itptit@gmail.com>
2024-10-16 11:22:33 +0700

    authentication middleware

--- src/routes/access/index.ts
+++ src/routes/access/index.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express'
 import { validateSchema } from '@/middlewares/validator.middleware'
+import { checkAuthentication, checkAuthorization } from '@/middlewares/auth.middleware'
 import accessController from '@/controllers/access.controller'
 import { loginReqBody, registerReqBody } from '@/shared/validators/access.validator'
+import { authReqHeaders } from '@/shared/validators/auth.validator'
 import asyncHandler from '@/shared/helpers/async-handler'
 
 const accessRoutes = Router()
 
 accessRoutes.post('/register', validateSchema(registerReqBody, 'body'), asyncHandler(accessController.register))
 accessRoutes.post('/login', validateSchema(loginReqBody, 'body'), asyncHandler(accessController.login))
+
+/** Check authentication */
+accessRoutes.use(validateSchema(authReqHeaders, 'headers'), checkAuthentication)
+accessRoutes.use(checkAuthorization(['user']))
 
 export default accessRoutes
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,14 +1,20 @@
 export enum RequestHeaders {
-  API_KEY = 'x-api-key'
+  API_KEY = 'x-api-key',
+  USER_ID = 'x-user-id',
+  ACCESS_TOKEN = 'authorization',
+  REFRESH_TOKEN = 'x-refresh-token'
 }
 
 export enum ErrorMessages {
   VALIDATION_ERROR = 'Validation error',
+  JWT_ERROR = 'Jwt error',
   API_KEY_NOT_FOUND = 'Api key not found',
   NOT_ALLOW_ACCESS_RESOURCE = 'Not allow access resource',
   EMAIL_ALREADY_REGISTERED = 'Email already registered',
   EMAIL_NOT_REGISTERED = 'Email not registered',
-  INVALID_CREDENTIALS = 'Invalid credentials'
+  INVALID_CREDENTIALS = 'Invalid credentials',
+  KEY_STORE_NOT_FOUND = 'Key store not found',
+  INVALID_AUTH_TOKEN = 'Invalid auth token'
 }
 
 export enum SuccessMessages {
