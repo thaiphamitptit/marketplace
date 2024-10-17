8363549a2037232bda81d5035b827b30df06fb54
thaiphamitptit <phamthai.itptit@gmail.com>
2024-10-17 12:15:21 +0700

    update category

--- src/routes/category/index.ts
+++ src/routes/category/index.ts
@@ -2,7 +2,11 @@ import { Router } from 'express'
 import { validateSchema } from '@/middlewares/validator.middleware'
 import { checkAuthentication, checkAuthorization } from '@/middlewares/auth.middleware'
 import categoryController from '@/controllers/category.controller'
-import { createNewCategoryReqBody } from '@/shared/validators/category.validator'
+import {
+  createNewCategoryReqBody,
+  updateCategoryReqBody,
+  updateCategoryReqParams
+} from '@/shared/validators/category.validator'
 import { authReqHeaders } from '@/shared/validators/auth.validator'
 import asyncHandler from '@/shared/helpers/async-handler'
 
@@ -16,6 +20,12 @@ categoryRoutes.post(
   '',
   validateSchema(createNewCategoryReqBody, 'body'),
   asyncHandler(categoryController.createNewCategory)
+)
+categoryRoutes.patch(
+  '/:categoryId',
+  validateSchema(updateCategoryReqParams, 'params'),
+  validateSchema(updateCategoryReqBody, 'body'),
+  asyncHandler(categoryController.updateCategory)
 )
 
 export default categoryRoutes
--- src/shared/dtos/category.dto.ts
+++ src/shared/dtos/category.dto.ts
@@ -1,4 +1,4 @@
-import { ICreateNewCategoryDto } from '@/shared/types/category'
+import { ICreateNewCategoryDto, IUpdateCategoryDto } from '@/shared/types/category'
 
 export class CreateNewCategoryDto {
   parent?: string | null
@@ -9,6 +9,24 @@ export class CreateNewCategoryDto {
   description?: string
 
   constructor({ parent, left, right, name, thumb, description }: ICreateNewCategoryDto) {
+    this.parent = parent
+    this.left = left
+    this.right = right
+    this.name = name
+    this.thumb = thumb
+    this.description = description
+  }
+}
+
+export class UpdateCategoryDto {
+  parent?: string | null
+  left?: number
+  right?: number
+  name?: string
+  thumb?: string
+  description?: string
+
+  constructor({ parent, left, right, name, thumb, description }: IUpdateCategoryDto) {
     this.parent = parent
     this.left = left
     this.right = right
