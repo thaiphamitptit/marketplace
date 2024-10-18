df624e7adfc59cccb726ea2009f536845e5f33ab
thaiphamitptit <phamthai.itptit@gmail.com>
2024-10-18 20:42:09 +0700

    get attributes

--- src/shared/validators/attribute.validator.ts
+++ src/shared/validators/attribute.validator.ts
@@ -23,3 +23,25 @@ export const deleteAttributeReqParams = Joi.object({
 export const getAttributeReqParams = Joi.object({
   attributeId: Joi.string().uuid().required()
 })
+
+export const getAttributesReqQuery = Joi.object({
+  filter: Joi.object({
+    type: Joi.string().valid('string', 'number', 'date', 'boolean', 'array', 'object').optional(),
+    createdAt: Joi.object({
+      $gte: Joi.date().iso().optional(),
+      $lte: Joi.date().iso().optional()
+    }).optional(),
+    updatedAt: Joi.object({
+      $gte: Joi.date().iso().optional(),
+      $lte: Joi.date().iso().optional()
+    }).optional()
+  }).optional(),
+  page: Joi.number().integer().optional(),
+  limit: Joi.number().integer().optional(),
+  sort: Joi.string().valid('name', 'createdAt', 'updatedAt').optional(),
+  order: Joi.string().valid('asc', 'desc').optional(),
+  select: Joi.array()
+    .items(Joi.string().valid('slug', 'name', 'type', 'description').optional())
+    .unique()
+    .optional()
+})
